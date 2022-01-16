78c3fcca3c3074bdbeb217ed9de676ca1588f698
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2022-01-16 22:50:51 +0000

    stop test from continuous running

--- app.test.js
+++ app.test.js
@@ -6,6 +6,7 @@ describe('GET /notes', () => {
     test('should respond with a 200 status code', async () => {
       const response = await request(app).get('/');
       expect(response.statusCode).toBe(200);
+      done();
     });
   });
 });
