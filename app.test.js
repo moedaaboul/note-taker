3fb5abc459749b1aaab09d2ebcdc5cd506e94a6c
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2022-01-16 23:20:50 +0000

    adds --forceexit to terminate test

--- app.test.js
+++ app.test.js
@@ -3,10 +3,9 @@ const request = require('supertest');
 
 describe('GET /notes', () => {
   describe('given an app', () => {
-    test('should respond with a 200 status code', async () => {
+    it('should respond with a 200 status code', async () => {
       const response = await request(app).get('/');
       expect(response.statusCode).toBe(200);
-      done();
     });
   });
 });
