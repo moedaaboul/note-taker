0c366803ca0ef4931cd6f5c430ff1749b3ce22c5
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2022-01-23 12:56:32 +0000

    updates app.test.js with additional route tests

--- app.test.js
+++ app.test.js
@@ -1,11 +1,75 @@
 const app = require('./index.js');
 const request = require('supertest');
 
-describe('GET /notes', () => {
-  describe('given an app', () => {
-    it('should respond with a 200 status code', async () => {
-      const response = await request(app).get('/');
-      expect(response.statusCode).toBe(200);
-    });
+describe('GET /notes', function () {
+  it('respond with json containing a list of all notes', function (done) {
+    request(app)
+      .get('/api/notes')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  });
+});
+
+// describe('GET *', function () {
+//   it('respond with json containing a list of all notes', function (done) {
+//     request(app).get('/').expect('Content-Type', 'text/html').end(done);
+//   });
+// });
+
+describe('POST /users', function () {
+  let data = {
+    title: 'This is a dummy title',
+    text: 'This is a dummy text',
+  };
+  it('respond with 201 created', function (done) {
+    request(app)
+      .post('/api/notes')
+      .send(data)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(201)
+      .end((err) => {
+        if (err) return done(err);
+        done();
+      });
+  });
+});
+
+describe('POST /users', function () {
+  let data = {
+    title: 'This is a dummy title',
+    dummy: 'This is a dummy text',
+  };
+  it('respond with 500 INTERNAL SERVER ERROR when "text" field is missing', function (done) {
+    request(app)
+      .post('/api/notes')
+      .send(data)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(500)
+      .end((err) => {
+        if (err) return done(err);
+        done();
+      });
+  });
+});
+
+describe('POST /users', function () {
+  let data = {
+    dummy: 'This is a dummy title',
+    text: 'This is a dummy text',
+  };
+  it('respond with 500 INTERNAL SERVER ERROR when "title" field is missing', function (done) {
+    request(app)
+      .post('/api/notes')
+      .send(data)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(500)
+      .end((err) => {
+        if (err) return done(err);
+        done();
+      });
   });
 });
